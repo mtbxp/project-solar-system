6d343182b655cb39192d31d4ff7d0245d3d42fe6
mtbxp <marcellotb2@gmail.com>
2022-06-21 19:26:58 -0300

    Requisito 6 e 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
 
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Title headline="Teste" />
+        <Missions />
       </>
     );
   }
